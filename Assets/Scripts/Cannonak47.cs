79fa59dea08acfc12041cc0cde499859e69383a8
janli <jan.lidauer@htl-wels.at>
2025-05-15 11:17:02 +0200

    ng

---
+++ Assets/Scripts/Cannonak47.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannonak47 : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float fireDelay = 0.5f; // Verzögerung in Sekunden
+    private float nextFireTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fire Projectile
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= nextFireTime)
+        {
+            Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, 0));
+            nextFireTime = Time.time + fireDelay;
+        }
+    }
+}
